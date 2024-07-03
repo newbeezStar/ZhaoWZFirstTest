623d7bc75fd40ca4e06df68849a0bb440f9d6735
ZhaoWZ <zwz15340408565@163.com>
2024-07-03 23:08:20 +0800

    test2

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue + 测试2" />
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Vant from 'vant';
+import 'vant/lib/index.css'; // 引入 Vant 样式文件
 
-createApp(App).mount('#app')
+createApp(App).use(Vant).mount('#app')
+
